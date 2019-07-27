eb21f18e5c29d9e0128e8a3e7967fd769118a32c
Sachin Boob <sachin.boob@jadeglobal.com>
2019-07-27 22:01:44 +0530

    Added code for tuples

---
+++ tuple.py
@@ -0,0 +1,16 @@
+import sys
+
+tuple1 = ("sachin","abhijeet","ganesh")
+
+print(tuple1)
+
+print(tuple1[0])
+
+#Below code throws - IndexError: tuple index out of range
+#print(tuple1[3])
+
+tuple2 = ("shivani","vaijayanti")
+
+tuple3 = tuple1 + tuple2
+
+print(tuple3)
