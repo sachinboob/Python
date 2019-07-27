1be53f2b6c89b9d07b915d075f554a26d92e0cf7
Sachin Boob <sachin.boob@jadeglobal.com>
2019-07-27 22:00:01 +0530

    Modified code in dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -2,11 +2,16 @@ import sys;
 
 students = {"Sachin" : 45, "Abhijeet": 55, "Ganesh" : 67};
 
+print("Length of students dictionary :- " + str(len(students)))
+
 print (students.keys());
 
 print(students.get("Sachin"));
 print(students["Ganesh"]);
 
 del students["Ganesh"]
+
+#Below code throws error - KeyError: 'Ganesh'
+print(students["GaMesh"])
 
 print("Length of students dictionary :- " + str(len(students)))
